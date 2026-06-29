627c0d27ddaad350c4b479461ef5b0ca8fc39b1f
peter.id <boss@peter.id>
2026-06-29 11:30:27 +0700

    Fix MLflow tracking URI connection refused in CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -14,7 +14,7 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     # Set tracking URI to localhost
-    mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+    # mlflow.set_tracking_uri("http://127.0.0.1:5000/")
     mlflow.set_experiment("Titanic_Prediction_Basic")
     
     # Enable autolog
